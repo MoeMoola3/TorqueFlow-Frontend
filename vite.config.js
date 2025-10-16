fe0bc98b02c158d127dabdeee3caddd816de880d
Mohammed Moola <moemooladev@gmail.com>
2025-10-16 18:16:40 +0200

    add github pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TorqueFlow-Frontend/',
   plugins: [react()],
+  server: {
+  proxy: {
+    // example: forward /ws to your backend websocket server
+    '/wss': {
+      target: 'wss://obd-api.moemoola.com/', // change port to your backend websocket port
+      ws: true,
+      changeOrigin: true
+    },
+    // forward API calls
+    '/api': {
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+      secure: false
+    }
+  }
+}
 })
